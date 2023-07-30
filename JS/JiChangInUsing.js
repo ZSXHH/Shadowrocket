e2a64cd3c5d7be8a065a05e0866f8869f14b2b97
ZSXHH <127708283+ZSXHH@users.noreply.github.com>
2023-07-30 23:04:58 +0800

    Update JiChangInUsing.js

--- JS/JiChangInUsing.js
+++ JS/JiChangInUsing.js
@@ -1,15 +1,22 @@
-#TG频道：https://t.me/iixiaobai
+//TG频道：https://t.me/iixiaobai
+//在下方 '' 中内容替换为：前缀 订阅链接
 var APs = [
-'aaa ',
-'bbb ',
-'ccc ',
-'ddd ',
-'eee ',
+'aaa',
+'bbb',
+'ccc',
+'ddd',
+'eee',
 ];
 
 //console.log(APs);
+
+//以&将上方数组合并为字符串，并赋值给line
 var line = APs.join('&');
 
-//console.log(APs);
+//console.log(line);
+
+//将line即字符串写入Line中存储
 $persistentStore.write(line,'Line');
-$done({});
+
+//结束
+$done();
